d3f3148116263d68131bfbcc5c7d96e75ae8b1e5
Rogetz <ronnyogetaz@gmail.com>
2023-12-07 12:05:40 +0300

    added the public folder

---
+++ public/javascripts/errorHandler.js
@@ -0,0 +1,7 @@
+let logoDiv = document.querySelector(".logo-text")
+let secondText = document.querySelector(".second-text")
+
+setTimeout(function(){
+    logoDiv.classList.add("active")
+    secondText.classList.add("active")
+},100)
